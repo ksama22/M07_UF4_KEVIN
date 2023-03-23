683268301e4d7d6993c2853cf5287b43c47db0d8
ksama22 <ksama22@jaumebalmes.net>
2023-03-23 20:50:03 +0100

    testing with 'dades'

--- jaumebalmes_kevin/projecte_m07/views.py
+++ jaumebalmes_kevin/projecte_m07/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import Context, loader
 # Create your views here.
 #Fem referencia al urls.py
 def index(request):
-    return HttpResponse("Error418, dame cafe")
+    profe = {"name":"Roger","surname":"Sobrino","age":"17"}
+    #Carrega el template de 'jaumebalmes_kevin\jaumebalmes_kevin\templates\head\index.html''
+    template = loader.get_template('index.html')
+    dades = template.render({"name":profe["name"],"surname":profe["surname"], "age":profe["age"]})
+    return HttpResponse(template.render(dades))
