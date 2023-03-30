c314d0e4b2049f7ea071c5dd84909cbefc2d76a2
ksama22 <ksama22@jaumebalmes.net>
2023-03-30 19:20:07 +0200

    index with parametres

--- jaumebalmes_kevin/projecte_m07/views.py
+++ jaumebalmes_kevin/projecte_m07/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 # Create your views here.
-#Fem referencia al urls.py
-#def index(request):
-#    profe = {"name":"Roger","surname":"Sobrino","age":"17"}
-#    #Carrega el template de 'jaumebalmes_kevin\jaumebalmes_kevin\templates\head\index.html''
- #   template = loader.get_template('index.html')
- #   dades = template.render({"name":profe["name"],"surname":profe["surname"], "age":profe["age"]})
-#    return HttpResponse(template.render(dades))
 
 
+#Fem referencia 'jaumebalmes_kevin\projecte_m07\urls.py'
+#Carrega les dades al html indicat
 def index(request):
-    return HttpResponse("Faro?")
+    profe = {"name":"Roger","surname":"Sobrino","age":"17"}
+    #Carrega el template de 'jaumebalmes_kevin\jaumebalmes_kevin\templates\head\index.html''
+    template = loader.get_template('index.html')
+    dades = template.render({"name":profe["name"],"surname":profe["surname"], "age":profe["age"]})
+    return HttpResponse(dades)
+
+#Carrega una paraula en concret
+#def index(request):
+#    return HttpResponse("Hola?")
+
+#Carrega un index.html de 'jaumebalmes_kevin\projecte_m07\templates\index.html'
+#def index(request):
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render())
