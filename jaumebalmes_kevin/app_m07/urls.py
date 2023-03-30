dc95d178614555b0fd762198a6852444a9850e1a
ksama22 <ksama22@jaumebalmes.net>
2023-03-30 20:45:27 +0200

    Load data in '/students' & '/teachers'

--- jaumebalmes_kevin/app_m07/urls.py
+++ jaumebalmes_kevin/app_m07/urls.py
@@ -4,5 +4,8 @@ from. import views
 
 #Creada per conectar amb views.py
 urlpatterns =[
-    path('', views.index,name='index')
+    path('', views.index,name='index'),
+    path('students', views.students,name='students'),
+    path('teachers', views.teachers,name='teachers')
+
 ]
--- jaumebalmes_kevin/app_m07/views.py
+++ jaumebalmes_kevin/app_m07/views.py
@@ -21,3 +21,18 @@ def index(request):
 #def index(request):
 #     template = loader.get_template('index.html')
 #     return HttpResponse(template.render())
+
+def users(request):
+    a = True
+    context = {'a':a}
+    return render(request, 'users.html',context)
+
+def teachers(request):
+    teachersList = [  {"id": 1,"name": "Juan Perez","course": "Matematicas"  },  {"id": 2,"name": "Maria Garcia","course": "Historia"  },  {"id": 3,"name": "Pedro Gomez","course": "Ciencias"  },  {"id": 4,"name": "Ana Lopez","course": "Literatura"  },  {"id": 5,"name": "Carlos Sanchez","course": "Ingles"  },  {"id": 6,"name": "Laura Torres","course": "Educacion Fisica"  },  {"id": 7,"name": "Javier Hernandez","course": "Filosofia"  },  {"id": 8,"name": "Silvia Ruiz","course": "Quimica"  },  {"id": 9,"name": "Antonio Martinez","course": "Geografia"  },  {"id": 10,"name": "Isabel Torres","course": "Arte"  }]
+    context = {"tchrs": teachersList}
+    return render(request, 'teachers.html', context)
+
+def students(request):
+    studentList = [  {"id": 1,"name": "Juan Garcia","age": 17  },  {"id": 2,"name": "Maria Lopez","age": 18  },  {"id": 3,"name": "Pedro Gomez","age": 16  },  {"id": 4,"name": "Ana Ruiz","age": 17  },  {"id": 5,"name": "Carlos Sanchez","age": 18  },  {"id": 6,"name": "Laura Torres","age": 16  },  {"id": 7,"name": "Javier Hernandez","age": 17  },  {"id": 8,"name": "Silvia Flores","age": 16  },  {"id": 9,"name": "Antonio Martinez","age": 18  },  {"id": 10,"name": "Isabel Torres","age": 17  }]
+    context = {"stdnts": studentList}
+    return render(request, 'students.html',context)
