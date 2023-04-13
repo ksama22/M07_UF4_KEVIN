803de80011969174e4e593694cdbe2ba392b1a32
Kevin Sama <113372511+ksama22@users.noreply.github.com>
2023-04-13 19:23:21 +0200

    Mvt kevin (#3)
    * Create database & migrate
    
    * Finished 'models' to 'ddbb'

--- jaumebalmes_kevin/app_m07/models.py
+++ jaumebalmes_kevin/app_m07/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+
+#Creado Teacher model for migrate
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+
+
+#Creado Student model for migrate
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+## Despres de canviar
+# python .\jaumebalmes_kevin\manage.py makemigrations
+# python .\jaumebalmes_kevin\manage.py migrate
