cf34a2691cfba733738f76e387ad2c999e6811a2
ksama22 <ksama22@jaumebalmes.net>
2023-04-13 19:20:50 +0200

    Finished 'models' to 'ddbb'

--- jaumebalmes_kevin/app_m07/models.py
+++ jaumebalmes_kevin/app_m07/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 #Creado Teacher model for migrate
 class Teacher(models.Model):
-    name = models.CharField
-    course = models.CharField
+    name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+
 
 #Creado Student model for migrate
 class Student(models.Model):
-    name = models.CharField
-    age = models.IntegerField
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
 
 ## Despres de canviar
 # python .\jaumebalmes_kevin\manage.py makemigrations
