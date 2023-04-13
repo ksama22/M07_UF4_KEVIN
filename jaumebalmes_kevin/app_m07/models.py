a76c0cc78ef3d7e23142d465e7e6870e1b6f01e4
ksama22 <ksama22@jaumebalmes.net>
2023-04-13 20:41:39 +0200

    Added 6 fields on form

--- jaumebalmes_kevin/app_m07/models.py
+++ jaumebalmes_kevin/app_m07/models.py
@@ -4,13 +4,21 @@ from django.db import models
 #Creado Teacher model for migrate
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
 
 
 #Creado Student model for migrate
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
     age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.CharField(max_length=100)
 
 ## Despres de canviar
 # python .\jaumebalmes_kevin\manage.py makemigrations
